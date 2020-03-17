d8864bebd8a29041cb10ba15b41bd585b6ad3938
samratashoka007 <61857788+samratashoka007@users.noreply.github.com>
2020-03-17 18:41:13 +0530

    modified in mainactivity

--- app/src/main/java/com/websquareit/daasset/HomeActivity.java
+++ app/src/main/java/com/websquareit/daasset/HomeActivity.java
@@ -1,7 +1,10 @@
 package com.websquareit.daasset;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
@@ -22,6 +25,7 @@ public class HomeActivity extends AppCompatActivity implements OnClickListener {
     /* access modifiers changed from: protected */
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //new LongOperation().execute("");
         setContentView((int) R.layout.activity_home);
         this.btn_new = (Button) findViewById(R.id.home_new);
         this.btn_save = (Button) findViewById(R.id.home_save);
@@ -31,6 +35,7 @@ public class HomeActivity extends AppCompatActivity implements OnClickListener {
         this.btn_save.setOnClickListener(this);
         this.btn_export1.setOnClickListener(this);
         this.btn_export.setOnClickListener(this);
+
     }
 
     public void onClick(View v) {
@@ -47,6 +52,53 @@ public class HomeActivity extends AppCompatActivity implements OnClickListener {
 
         }
     }
+    @Override
+    protected void onStart() {
+
+        super.onStart();
+
+        //Handler handler = new Handler();
+        final Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                //Write whatever to want to do after delay specified (1 sec)
+                Log.d("Handler", "Running Handler");
+            }
+        }, 1000);
+
+    }
+   /* private class LongOperation extends AsyncTask<String, Void, String> {
+
+        @Override
+        protected String doInBackground(String... params) {
+            for(int i=0;i<5;i++) {
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+            }
+
+            return "Executed";
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            //TextView txt = (TextView) findViewById(R.id.output);
+            //txt.setText("Executed"); // txt.setText(result);
+            //might want to change "executed" for the returned string passed into onPostExecute() but that is upto you
+        }
+
+        @Override
+        protected void onPreExecute() {
+        }
+
+        @Override
+        protected void onProgressUpdate(Void... values) {
+        }
+    }*/
 
     public void onBackPressed() {
         finish();
